8ba4b235c11a7ff5fe707f001a27f0ee9d2b77ff
AkshayRGhatge <akshayghatge6011@gmail.com>
2024-08-04 13:43:25 -0400

    Initial commit for the sql db

---
+++ Database1/dbo/Tables/users.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[users] (
+    [userid]       INT          IDENTITY (1, 1) NOT NULL,
+    [name]         VARCHAR (50) NULL,
+    [email]        VARCHAR (50) NULL,
+    [city]         VARCHAR (50) NULL,
+    [identitycode] VARCHAR (50) NULL,
+    [phoneNum] NCHAR(10) NULL, 
+    CONSTRAINT [PK_users] PRIMARY KEY CLUSTERED ([userid] ASC)
+);
+
